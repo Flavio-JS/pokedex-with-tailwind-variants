b842129f713c5b9511b0e02f4b3e331c0118b116
flavio.filho <flavio.filho@trinusco.com.br>
2023-12-27 20:50:59 +0000

    fixed color tokens fixed pixels of .styles files

--- src/components/PokedexListHomePage/PokedexListHeader/PokedexListHeader.styles.ts
+++ src/components/PokedexListHomePage/PokedexListHeader/PokedexListHeader.styles.ts
@@ -3,9 +3,9 @@ import { tv } from "tailwind-variants";
 export const pokedexListHeaderStyles = tv({
   slots: {
     pokedexListHeaderWrapper:
-      "flex flex-col items-start p-12 bg-identity-primary w-full",
+      "flex flex-col items-start p-[12px] bg-identity-primary w-full",
     pokedexListHeaderFilterWrapper: "flex justify-between items-center w-full",
     pokedexListHeaderSortWrapper:
-      "flex gap-8 items-center text-grayscale-white",
+      "flex gap-[8px] items-center text-grayscale-white",
   },
 });
--- src/components/PokedexListHomePage/PokedexListHomePage.styles.ts
+++ src/components/PokedexListHomePage/PokedexListHomePage.styles.ts
@@ -3,8 +3,8 @@ import { tv } from "tailwind-variants";
 export const pokedexListStyles = tv({
   slots: {
     pokedexListWrapper:
-      "flex flex-col items-start p-4 bg-identity-primary h-screen w-screen overflow-y-hidden",
+      "flex flex-col items-start p-[4px] bg-identity-primary h-screen w-screen overflow-y-hidden",
     pokedexListPokemonCardWrapper:
-      "flex flex-row flex-wrap w-full h-calcvh-84 gap-10 p-10 bg-grayscale-white justify-between content-start rounded-8 overflow-y-scroll scrollbar",
+      "flex flex-row flex-wrap w-full h-calcvh-[84px] gap-[10px] p-[10px] bg-grayscale-white justify-between content-start rounded-[8px] overflow-y-scroll scrollbar",
   },
 });
